2585ee3dd77cba59475c2914864b7742998a0cce
Marko <markgufman21@gmail.com>
2025-09-14 01:41:02 +0200

    Enhance Hero component text styling by italicizing "Grow Your Business" for improved emphasis and visual appeal.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,8 @@ export default function Hero() {
     <div className="flex flex-col items-center gap-12  pt-18 px-4">
       <div className="flex flex-col items-center gap-4">
         <h1 className="text-center text-4xl font-bold">
-          Websites Built to Grow Your Business and Drive Revenue
+          Websites Built to <span className="italic">Grow Your Business</span>{' '}
+          and Drive Revenue
         </h1>
         <p className="text-center text-sm">
           Modern, conversion-focused websites that turn visitors into customers.
