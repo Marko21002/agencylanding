91747e1e4aff4127a094fab0e4b950dac0614b0c
Marko <markgufman21@gmail.com>
2025-09-14 02:49:24 +0200

    Update Hero component styling by adding a background color, adjusting button colors, and incorporating an image for enhanced visual appeal and user engagement.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center gap-12  pt-18 px-4">
+    <div className="flex flex-col items-center gap-12  pt-18 px-4 mx-2 bg-[#000E2B] text-white rounded-lg">
       <div className="flex flex-col items-center gap-4">
         <h1 className="text-center text-4xl font-bold">
           Websites Built to <span className="italic">Grow Your Business</span>{' '}
@@ -10,15 +10,18 @@ export default function Hero() {
           Modern, conversion-focused websites that turn visitors into customers.
         </p>
         <div className="w-full">
-          <button className="w-full py-3 bg-black text-white">
+          <button className="w-full py-3 bg-white text-black rounded-lg">
             Book A Free Call
           </button>
         </div>
       </div>
       <div
         className="w-full h-[300px]
-       bg-gray-500"
-      ></div>
+       "
+      >
+        {' '}
+        <img src="/laptop.png" alt="" className="w-full h-full object-cover" />
+      </div>
     </div>
   )
 }
